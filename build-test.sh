a50d3fe8a12e4471fe82b308ff503929b34ba063
Xinliang Liu <xinliang.liu@linaro.org>
2016-04-21 17:20:39 +0800

    Add build-test.sh script
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

---
+++ build-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash                                                                                                                                                                        
+KERNEL_OUT=~/work/kernel/out
+BUILD_TARGET="Image hisilicon/hip05-d02.dtb hisilicon/hip06-d03.dtb"
+CROSS_TOOLCHAIN=aarch64-linux-gnu-
+
+set -x
+
+make -p ${KERNEL_OUT}
+make mrproper
+
+## build kernel testing
+make ARCH=arm64 O=${KERNEL_OUT} defconfig &&
+make ARCH=arm64 CROSS_COMPILE=$CROSS_TOOLCHAIN O=${KERNEL_OUT} \
+${BUILD_TARGET} -j40
+
+set +x
