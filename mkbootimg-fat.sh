0e55c4be05ec4955a08b1682cf42d7f34d6777f7
Xinliang Liu <xinliang.liu@linaro.org>
2016-05-18 09:23:06 +0800

    update mkbootimg-fat.sh
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- mkbootimg-fat.sh
+++ mkbootimg-fat.sh
@@ -13,24 +13,36 @@ CROSS_TOOLCHAIN=aarch64-linux-gnu-
 if [ "$BUILD" = "rp" ]; then
 	IP=192.168.1.107
 	make ARCH=arm64 O=${OUT} defconfig
-	CP=scp
-	DEST=${USER}@${IP}:work/img/${BUILD}
-	DTB="hisilicon/hip05-d02.dtb"
-	#DTB2="hisilicon/hip06-d03.dtb"
-elif [ "$BUILD" = "debian" ] || [ "$BUILD" = "oe" ]; then
-	FLAGS=
-	export LOCALVERSION="-linaro-hikey"
-	make ARCH=arm64 O=${OUT} defconfig
 
 	#./scripts/kconfig/merge_config.sh -m arch/arm64/configs/defconfig \
-	#arch/arm64/configs/hikey_defconfig && 
+	#arch/arm64/configs/distro.config && 
 	#mv -f .config ${OUT}/.merged.config &&
 	#make ARCH=arm64 CROSS_COMPILE=$CROSS_TOOLCHAIN O=${OUT} \
 	#KCONFIG_ALLCONFIG=${OUT}/.merged.config alldefconfig
 
 	CP=scp
+	DEST=${USER}@${IP}:ftp
+	DTB="hisilicon/hip05-d02.dtb"
+	DTB2="hisilicon/hip06-d03.dtb"
+	BUILD_TARGETS="Image $DTB $DTB2"
+	CP_TARGETS="arch/arm64/boot/Image arch/arm64/boot/dts/$DTB arch/arm64/boot/dts/$DTB2"
+elif [ "$BUILD" = "debian" ] || [ "$BUILD" = "oe" ]; then
+	FLAGS=
+	export LOCALVERSION="-linaro-hikey"
+	#make ARCH=arm64 O=${OUT} defconfig
+
+	./scripts/kconfig/merge_config.sh -m arch/arm64/configs/defconfig \
+	arch/arm64/configs/distro.config && 
+	mv -f .config ${OUT}/.merged.config &&
+	make ARCH=arm64 CROSS_COMPILE=$CROSS_TOOLCHAIN O=${OUT} \
+	KCONFIG_ALLCONFIG=${OUT}/.merged.config alldefconfig
+
+
+	CP=scp
 	DEST=${USER}@${IP}:work/img/${BUILD}
 	DTB="hisilicon/hi6220-hikey.dtb"
+	BUILD_TARGETS="Image $DTB" # modules INSTALL_MOD_PATH=$OUT modules_install"
+	CP_TARGETS="arch/arm64/boot/Image arch/arm64/boot/dts/$DTB"
 elif [ "$BUILD" = "aosp" ]; then
 	#FLAGS="KCFLAGS=-fno-pic"
 
@@ -46,13 +58,12 @@ fi
 
 ## kernel compile
 if [ $? -eq 0 ]; then 
-	# modules INSTALL_MOD_PATH=$out modules_install hisilicon/ 
+	# modules INSTALL_MOD_PATH=$OUT modules_install hisilicon/ 
 
 	make ARCH=arm64 CROSS_COMPILE=$CROSS_TOOLCHAIN O=${OUT} ${FLAGS} \
-		${DTB} ${DTB2} Image -j40 &&
+		${BUILD_TARGETS} -j40 &&
 	cd ${OUT} &&
-	${CP} -r arch/arm64/boot/Image arch/arm64/boot/dts/${DTB} \
-	${DEST}
+	${CP} -r ${CP_TARGETS} ${DEST}
 
 	if [ "$BUILD" = "aosp" ] && [ $? -eq 0 ]; then 
 		cd ${AOSP_SRC} &&
