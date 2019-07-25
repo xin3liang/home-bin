c2dde02410903617937ee2a118f67222341a0e11
Xinliang Liu <xinliang.liu@linaro.org>
2019-07-25 14:23:55 +0800

    sync with native build script
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,5 +1,13 @@
 #!/bin/bash                                                                                                                                                                        
 
+## params
+version_prefix="test"
+
+if [ $# -ge 1 ]; then
+    version_prefix=$1
+fi
+
+
 OUT=~/work/kernel/out
 KERNEL_BOOT_DIR=arch/arm64/boot
 CP=cp
@@ -8,11 +16,11 @@ DEST=~/tftp
 CROSS_TOOLCHAIN=aarch64-linux-gnu-
 COFNIG_FILE=~/config-4.16.0-estuary.5.aarch64
 
-export LOCALVERSION="-liuxl-test-`date +%F`"
+export LOCALVERSION="-${version_prefix}-`date +%F`"
 export KBUILD_DEBARCH=arm64
 # targets: bindeb-pkg binrpm-pkg INSTALL_MOD_STRIP=1 Image modules INSTALL_MOD_PATH=${OUT} modules_install"
-BUILD_TARGETS="Image modules" 
-#BUILD_TARGETS="binrpm-pkg INSTALL_MOD_STRIP=1" 
+#BUILD_TARGETS="Image modules"
+BUILD_TARGETS="binrpm-pkg INSTALL_MOD_STRIP=1"
 CP_TARGETS="${KERNEL_BOOT_DIR}/Image"
 
 set -x
@@ -20,7 +28,7 @@ set -x
 ## kernel .config compile
 #cp ${COFNIG_FILE} ${OUT}/.config	
 # CONFIG: oldconfig defconfig estuary_defconfig
-CONFIG="estuary_defconfig"
+CONFIG="oldconfig"
 
 make ARCH=arm64 O=${OUT} ${CONFIG}
 
