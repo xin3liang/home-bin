e96c1e2cb7f58bf6dd7be983582163ebcda05f49
Xinliang Liu <xinliang.liu@linaro.org>
2020-02-13 07:58:25 +0000

    Spilt vim.sh into cscope-c.sh and cscope-python.sh

---
+++ cscope-c.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd `pwd`
+
+set -x
+
+DIR=.
+
+if [ -n "$1" ] ;then
+	DIR=$1
+fi
+
+ctags -R $DIR
+
+find $(pwd)/$DIR -type f \( -name "*.[ch]" -o -name "*.cpp" ! -path \
+"*test*" ! -path ".*" \) > cscope.files
+cscope -bkq -i cscope.files
+
+set +x
+
--- cscope-python.sh
+++ cscope-python.sh
@@ -10,12 +10,6 @@ if [ -n "$1" ] ;then
 	DIR=$1
 fi
 
-#ctags -R $DIR
-#
-#find $(pwd)/$DIR -type f \( -name "*.[ch]" -o -name "*.cpp" ! -path \
-#"*test*" ! -path ".*" \) > cscope.files
-#cscope -bkq -i cscope.files
-
 find $(pwd)/$DIR -type f \( -name "*.py" ! -path "*test*" ! -path ".*" \) > pycscope.files
 pycscope -i pycscope.files
 
