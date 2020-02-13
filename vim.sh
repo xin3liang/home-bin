92246b2f117b4a73d0b88a3eee0c62d1686f7cdc
Xinliang Liu <xinliang.liu@linaro.org>
2020-02-13 02:14:52 +0000

    Exclude .* path for python

--- vim.sh
+++ vim.sh
@@ -10,14 +10,14 @@ if [ -n "$1" ] ;then
 	DIR=$1
 fi
 
-ctags -R $DIR
+#ctags -R $DIR
+#
+#find $(pwd)/$DIR -type f \( -name "*.[ch]" -o -name "*.cpp" ! -path \
+#"*test*" ! -path ".*" \) > cscope.files
+#cscope -bkq -i cscope.files
 
-find $(pwd)/$DIR -type f \( -name "*.[ch]" -o -name "*.cpp" ! -path \
-"*test*" \) > cscope.files
-cscope -bkq -i cscope.files
-
-#find $(pwd)/$DIR -type f \( -name "*.py" ! -path "*test*" \) > pycscope.files
-#pycscope -i pycscope.files
+find $(pwd)/$DIR -type f \( -name "*.py" ! -path "*test*" ! -path ".*" \) > pycscope.files
+pycscope -i pycscope.files
 
 set +x
 
