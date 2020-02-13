68a6d8def3cf10b8786b68390af84945233c3db2
Xinliang Liu <xinliang.liu@linaro.org>
2020-02-13 02:18:28 +0000

    Add cscope-go.sh

---
+++ cscope-go.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# generate cscope index files in current directory
+# the generated cscope index files also include go standard packages
+
+#if [ "$GOROOT" = "" ]
+#then
+#echo "GOROOT is not set"
+#    exit 1
+#fi
+#
+#go_pkg_src=$GOROOT/pkg
+#
+#find $go_pkg_src -name "*.go" -print > cscope.files
+find . -name "*.go" -print > cscope.files
+
+if cscope -b -k; then
+echo "Done"
+else
+echo "Failed"
+    exit 1
+fi
