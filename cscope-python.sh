a1580bd1c2525fbf6c6e70ff3d2b76d840a1b09c
Xinliang Liu <xinliang.liu@linaro.org>
2020-06-15 00:19:00 +0000

    Make find handles links dir
    Signed-off-by: Xinliang Liu <xinliang.liu@linaro.org>

--- cscope-python.sh
+++ cscope-python.sh
@@ -10,7 +10,7 @@ if [ -n "$1" ] ;then
 	DIR=$1
 fi
 
-find $(pwd)/$DIR -type f \( ! -path "*test*" ! -path "*/.tox/*" ! -path "*/.git/*" -name "*.py"  \) > pycscope.files
+find -L $(pwd)/$DIR -type f \( ! -path "*test*" ! -path "*/.tox/*" ! -path "*/.git/*" -name "*.py"  \) > pycscope.files
 pycscope -i pycscope.files
 
 set +x
