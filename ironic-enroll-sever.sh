8d19cf71075073ef970c648f6332f142ee1d461b
Xinliang Liu <xinliang.liu@linaro.org>
2020-10-19 06:54:11 +0000

    Add BMC connectivity check

--- ironic-enroll-sever.sh
+++ ironic-enroll-sever.sh
@@ -1,13 +1,15 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
 # An input file should contain bellow format lines.
-# <Name> <MAC address> <BMC IP address> <BMC username> <BMC password>
-# Where Name is the server production name, e.g.: D05/THX2
-# MAC address is the provision interface's MAC.
-# 
+# <Model> <MAC address> <BMC IP address> <BMC username> <BMC password> <Boot option>
+# Where
+# <Model> is the server production name, e.g.: D05/THX1/THX2
+# <MAC address> is the provision interface's MAC.
+# <Boot option> is 'local' for local disk boot or 'netboot' for iscsi volume boot
 
 
 D05_CPUS=64
+THX1_CPUS=48
 THX2_CPUS=128
 RAM_MB=32768 #default RAM 32G
 DISK_GB=10 # default root disk 10G
@@ -17,6 +19,8 @@ function get_cpus
 {
     if [[ ${1,,} = "d05" ]]; then
         echo $D05_CPUS
+    elif [[ ${1,,} = "thx1" ]]; then
+        echo $THX1_CPUS
     elif [[ ${1,,} = "thx2" ]]; then
         echo $THX2_CPUS
     else
@@ -26,40 +30,83 @@ function get_cpus
 
 server_info_file=$1
 server_count=1
+fail_count=0
+fail_ips=""
 
+echo "Check BMC connectivity..."
 while read server_info; do
-    server_name=$(echo $server_info|awk '{print $1}')
+    bmc_ip=$(echo $server_info|awk '{print $3}')
+    bmc_user=$(echo $server_info|awk '{print $4}')
+    bmc_passwd=$(echo $server_info|awk '{print $5}')
+
+    ret=0
+    ipmitool -I lanplus -H $bmc_ip  -U $bmc_user -P $bmc_passwd  power status || ret=1
+    if [[ "$ret" = 0 ]]; then
+	echo "$bmc_ip ok"
+    else
+        echo "$bmc_ip fail"
+        fail_count=$((fail_count+1))
+        fail_ips+=" $bmc_ip"
+    fi
+done < $server_info_file
+
+if [[ $fail_count -gt 0 ]]; then
+	echo "Fail count: $fail_count."
+        echo "Please check fail BMC IP:"
+	for ip in $fail_ips; do
+	    echo $ip
+        done
+	echo "Check BMC connectivity finish."
+	exit 1
+fi
+echo "Check BMC connectivity finish."
+
+echo "Enroll server..."
+while read server_info; do
+    model=$(echo $server_info|awk '{print $1}')
     mac_addr=$(echo $server_info|awk '{print $2}')
     bmc_ip=$(echo $server_info|awk '{print $3}')
     bmc_user=$(echo $server_info|awk '{print $4}')
     bmc_passwd=$(echo $server_info|awk '{print $5}')
-    node_name=${server_name,,}-${server_count}
+    boot_option=$(echo $server_info|awk '{print $6}')
+    node_name=${model,,}-${server_count}
     connector_iqn="iqn.2017-05.org.openstack:node-$node_name"
-    cpus=$(get_cpus $server_name)
+    cpus=$(get_cpus $model)
 
     # if flavor not existed, 
     # then create it
-    flavor_name=bm-${server_name,,}
-    flavor=$(openstack flavor list|grep -i $flavor_name) || true
-    if [[ -z $flavor ]];then
-        resource_class=CUSTOM_${RESOURCE_CLASS^^}_${server_name^^}
+    flavor_name_prefix=bm-${model,,}
+    flavor=$(openstack flavor list|grep -i $flavor_name_prefix) || true
+    if [[ -z $flavor ]]; then
+        resource_class=CUSTOM_${RESOURCE_CLASS^^}_${model^^}
+	volume_boot_flavor_name=$flavor_name_prefix-volume-boot
+	local_boot_flavor_name=$flavor_name_prefix-local-boot
         openstack flavor create --ram $RAM_MB --vcpus $cpus \
-            --disk $DISK_GB  $flavor_name
-        openstack flavor set $flavor_name \
+            --disk $DISK_GB  $volume_boot_flavor_name
+        openstack flavor set $volume_boot_flavor_name \
             --property resources:$resource_class=1 \
             --property resources:VCPU=0 \
             --property resources:MEMORY_MB=0 \
             --property resources:DISK_GB=0 \
             --property capabilities:boot_option='netboot'
+        openstack flavor create --ram $RAM_MB --vcpus $cpus \
+            --disk $DISK_GB  $local_boot_flavor_name
+        openstack flavor set $local_boot_flavor_name \
+            --property resources:$resource_class=1 \
+            --property resources:VCPU=0 \
+            --property resources:MEMORY_MB=0 \
+            --property resources:DISK_GB=0 \
+            --property capabilities:boot_option='local'
     fi
 
     deploy_kernel_id=$(openstack image show bm-deploy-kernel \
                         -f value -c id)
     deploy_initrd_id=$(openstack image show bm-deploy-initrd \
                         -f value -c id)
-    resource_class=${RESOURCE_CLASS,,}-${server_name,,}
+    resource_class=${RESOURCE_CLASS,,}-${model,,}
 
-    # Note: Set capabilities=boot_option:local if it has disk.
+    # Note: Set capabilities=boot_option:local if it has disk for local disk boot
+    # and only one boot_option could be set once time.
     node_id=$(openstack baremetal node create \
         --name $node_name \
         --driver ipmi \
@@ -72,7 +119,7 @@ while read server_info; do
         --property cpu_arch=aarch64 \
         --property cpus=$cpus \
         --property memory_mb=$RAM_MB \
-        --property capabilities='boot_mode:uefi,iscsi_boot:True,boot_option:netboot' \
+        --property capabilities="boot_mode:uefi,iscsi_boot:True,boot_option:$boot_option" \
         --storage-interface cinder \
         --deploy-interface iscsi \
         -f value -c uuid)
@@ -88,5 +135,7 @@ while read server_info; do
     openstack baremetal node manage $node_id
     openstack baremetal node provide $node_id
 
+    echo "Server count: $server_count"
     server_count=$((server_count+1))
 done < $server_info_file
+echo "Enroll server finish."
